abf1df110d807b0cdcfc764b5f91991467c4cfd6
iwsmimsantos <158859354+iwsmimsantos@users.noreply.github.com>
2024-06-14 10:25:33 -0300

    Feat: Conserta erro em models e instala testes

--- api/models/Hero.js
+++ api/models/Hero.js
@@ -1,8 +1,14 @@
 module.exports = {
   attributes: {
     name: {
-      type: "",
-      required: true,
+      type: "string", /*
+       Encontrei um erro tentando dar sails lift que foi esse "In the `name` attribute of model `hero`:
+      Attributes must have either a `type` property that is a non-empty string declaring
+      the attribute's data type, a `model` property declaring a singular association with
+      another model, or a `collection` property declaring a plural assocation with another model."
+      required: true; 
+      
+      Então, estou consertando o model que não está especificando o tipo de dado que se espera. */
     },
     power: {
       type: "string",
